44e8a0493f42242b880f1a9b14be7ab38f3ad10d
Seth Giorgianni <sgiorgiann@my.gcu.edu>
2018-10-17 21:36:27 -0700

    Commiting All Files From Programming Exercise 1 For Review.

---
+++ Programming Exercise 1/Exercise1Part2.java
@@ -0,0 +1,13 @@
+//Seth Giorgianni this work is my own.
+//Program prints a few statements as outlined in Exercise 1 instructions.
+
+public class Exercise1Part2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("My name is Seth Giorgianni.");
+		System.out.println("My favorite color is blue.");
+		System.out.println("If I won the lottery, I would invest it all, and live off the interest.");
+	
+	}
+}
